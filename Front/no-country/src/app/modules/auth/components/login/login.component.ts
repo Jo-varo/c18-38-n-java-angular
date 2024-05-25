11511e3ddf01c310da4dcff54081738eacad0070
mjgonzalez59 <mjulissagonzalez@gmail.com>
2024-05-24 21:51:23 -0500

    Installing Angular Material

--- Front/no-country/src/app/modules/auth/components/login/login.component.ts
+++ Front/no-country/src/app/modules/auth/components/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [
+    MatSlideToggleModule
+  ]
 })
 export class LoginComponent implements OnInit {
 
