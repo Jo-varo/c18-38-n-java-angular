69b21379da850054f02d2830b880d5e4112448b7
mjgonzalez59 <mjulissagonzalez@gmail.com>
2024-05-29 21:05:01 -0500

    Creation home components

---
+++ Front/no-country/src/app/modules/home/components/search-bar/search-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
