ca3a230cc4bd5e4bd08332bb55f31c9f1084a8b0
jovaro <jorgevargas0240@gmail.com>
2024-06-04 17:52:02 -0500

    Changed datepicker format

--- Front/no-country/src/app/modules/home/components/search-bar/search-bar.component.ts
+++ Front/no-country/src/app/modules/home/components/search-bar/search-bar.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import * as _moment from 'moment';
+import { default as _rollupMoment } from 'moment';
+import 'moment/locale/es';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   standalone: true,
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css'],
-  providers: [provideNativeDateAdapter()],
+  providers: [
+    provideMomentDateAdapter(MY_FORMATS),
+    { provide: MAT_DATE_LOCALE, useValue: 'es' },
+  ],
   imports: [
     MatFormFieldModule,
     MatInputModule,
